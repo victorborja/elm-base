d2715f35e346adca0e937c051b8d7df1bc91f4f1
Victor Borja <vborja@apache.org>
2015-08-19 03:42:59 -0500

    Implement basic elm-architecture.
    https://github.com/evancz/elm-architecture-tutorial/
    https://github.com/evancz/start-app

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,5 +1,30 @@
-import Html exposing (text)
+import Html exposing (div, button, text)
+import Html.Events exposing (onClick)
+import StartApp.Simple as StartApp
 
-main : Html.Html
+main : Signal Html.Html
 main =
-  text "Hello World!"
+  StartApp.start { model = model, view = view, update = update }
+
+
+type alias Model = Int
+
+model : Model
+model = 0
+
+view : Signal.Address Action -> Model -> Html.Html
+view address model =
+  div []
+    [ button [ onClick address Decrement ] [ text "-" ]
+    , div [] [ text (toString model) ]
+    , button [ onClick address Increment ] [ text "+" ]
+    ]
+
+
+type Action = Increment | Decrement
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    Increment -> model + 1
+    Decrement -> model - 1
